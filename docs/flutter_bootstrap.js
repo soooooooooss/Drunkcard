d76c5d65dd1a8151617bc9604c122abf779aa3d4
soooooooooss <ikaisou511@gmail.com>
2025-09-22 21:37:49 +0900

    Fix base href for GitHub Pages (Drunkcard)

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2220156413"
+    serviceWorkerVersion: "859056235"
   }
 });
